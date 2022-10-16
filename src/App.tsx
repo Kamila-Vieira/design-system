f0e5e9b0fd6d280f038a38fdd853b5513277d0c4
Kamila Vieira <vkamila54@outlook.com>
2022-10-15 21:16:09 -0300

    FEAT: adding initial configs

---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+import "./styles/global.css";
+
+export function App() {
+  return <h1 className="">Hello</h1>;
+}
---
+++ tailwind.config.cjs
@@ -0,0 +1,36 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.tsx"],
+  theme: {
+    fontSize: {
+      xs: 14,
+      sm: 16,
+      md: 18,
+      lg: 20,
+      xl: 24,
+      "2xl": 32,
+    },
+    colors: {
+      transparent: "transparent",
+      black: "#000000",
+      white: "#ffffff",
+      gray: {
+        900: "#121214",
+        800: "#202024",
+        400: "#7C7C8A",
+        200: "#C4C4CC",
+        100: "#E1E1E6",
+      },
+      cyan: {
+        500: "#81D8F7",
+        300: "#8FDFFF",
+      },
+    },
+    extend: {
+      fontFamily: {
+        sans: "'Inter', sans-serif",
+      },
+    },
+  },
+  plugins: [],
+};
